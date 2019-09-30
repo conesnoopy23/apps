a88cc3f663c1fab45d473e16723cf862f3302bce
conesnoopy23 <matheus.santos.hcs@gmail.com>
2019-09-30 00:40:08 -0300

    Finish app

---
+++ back/src/controllers/Like.js
@@ -0,0 +1,37 @@
+const Model_dev = require('../models/Model_dev');
+
+module.exports = {
+    async store(req, res){
+
+        console.log(req.io, req.connectedUsers);
+
+        const {user}  = req.headers; 
+        const {devId} = req.params;
+
+        const loggedDev = await Model_dev.findById(user); 
+        const targetDev = await Model_dev.findById(devId);
+
+        if (!targetDev) {
+            return res.status(400).json({error: 'Dev not exists'});
+        } 
+
+        if (targetDev.likes.includes(loggedDev._id)) {
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
+        }
+
+        loggedDev.likes.push(targetDev._id);
+
+        await loggedDev.save()
+        
+        return res.json(loggedDev);
+    }
+};
--- mobile/App.js
+++
@@ -1,8 +0,0 @@
-import React, {Fragment} from 'react';
-import { View, Text } from 'react-native';
-
-export default function App() {
-  return (
-    <Text>Hello Gerson</Text>
-  );
-}
